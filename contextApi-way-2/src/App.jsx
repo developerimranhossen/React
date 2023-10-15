cb4eeb3796701eaa8a026603e5bd132863223715
Md. Imran Hossen <developerimranhossen@gmail.com>
2023-10-15 18:09:31 +0600

    finished

--- contextApi-way-2/src/App.jsx
+++ contextApi-way-2/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context'/theme";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightMode = () => {
+    setThemeMode("light")
+  }
+  const darkMode = () => {
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider value={{ themeMode, lightMode, darkMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <ThemeBtn />
+          <div className="w-full max-w-sm mx-auto"></div>
+          <Card />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
